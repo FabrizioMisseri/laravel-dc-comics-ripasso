536243254919e4d944669d493d74af9ec331ea96
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-06 18:45:26 +0100

    *

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -24,22 +24,22 @@
 
             <div>
                 <label for="title">title</label>
-                <input type="text" name="title" value=" {{ $comic->title }} " id="title">
+                <input type="text" name="title" value="{{ old('title', $comic->title) }}" id="title">
             </div>
 
             <div>
                 <label for="description">text</label>
-                <textarea name="description" id="description" cols="30" rows="10"> {{ $comic->description }} </textarea>
+                <textarea name="description" id="description" cols="30" rows="10"> {{ old('description', $comic->description) }} </textarea>
             </div>
 
             <div>
                 <label for="thumb">img path</label>
-                <input type="text" name="thumb" value=" {{ $comic->thumb }} " id="thumb">
+                <input type="text" name="thumb" value="{{ old('thumb', $comic->thumb) }}" id="thumb">
             </div>
 
             <div>
                 <label for="series">series</label>
-                <input type="text" name="series" value=" {{ $comic->series }} " id="series">
+                <input type="text" name="series" value="{{ old('series', $comic->series) }}" id="series">
             </div>
 
             <div>
@@ -49,7 +49,7 @@
 
             <div>
                 <label for="type">type</label>
-                <input type="text" name="type" value=" {{ $comic->type }} " id="type">
+                <input type="text" name="type" value="{{ old('type', $comic->type) }}" id="type">
             </div>
 
             <button type="submit" class="btn btn-primary mt-4">Edit</button>
